4b0d9a24c7f4b05d2a768461d2854a3e72643a9d
pzj <1538563097@qq.com>
2023-08-11 23:18:08 +0800

    feat：去掉 maven 配置

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -4,10 +4,7 @@ import java.util.*
 
 plugins {
     id("com.android.library")
-    id("maven-publish")
     kotlin("android")
-    `maven-publish`
-    signing
 }
 
 val versionName = "4.0.1.2"
@@ -42,14 +39,6 @@ android {
     }
     //namespace = "com.chad.library"
 
-
-/*    publishing {
-        singleVariant("release") {
-            // if you don't want sources/javadoc, remove these lines
-            withSourcesJar()
-            withJavadocJar()
-        }
-    }*/
 }
 
 
@@ -60,104 +49,3 @@ dependencies {
 
     implementation("androidx.databinding:databinding-runtime:4.2.2")
 }
-
-//---------- maven upload info -----------------------------------
-
-var signingKeyId = ""//签名的密钥后8位
-var signingPassword = ""//签名设置的密码
-var secretKeyRingFile = ""//生成的secring.gpg文件目录
-var ossrhUsername = ""//sonatype用户名
-var ossrhPassword = "" //sonatype密码
-
-
-val localProperties: File = project.rootProject.file("local.properties")
-
-if (localProperties.exists()) {
-    println("Found secret props file, loading props")
-    val properties = Properties()
-
-    InputStreamReader(FileInputStream(localProperties), Charsets.UTF_8).use { reader ->
-        properties.load(reader)
-    }
-    /*signingKeyId = properties.getProperty("signing.keyId")
-    signingPassword = properties.getProperty("signing.password")
-    secretKeyRingFile = properties.getProperty("signing.secretKeyRingFile")
-    ossrhUsername = properties.getProperty("ossrhUsername")
-    ossrhPassword = properties.getProperty("ossrhPassword")*/
-
-} else {
-    println("No props file, loading env vars")
-}
-
-afterEvaluate {
-
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                from(components.findByName("release"))
-                groupId = "io.github.cymchad"
-                artifactId = "BaseRecyclerViewAdapterHelper"
-                version = versionName
-
-                /*pom {
-                    name.value("BaseRecyclerViewAdapterHelper")
-                    description.value("Powerful and flexible RecyclerAdapter")
-                    url.value("https://github.com/CymChad/BaseRecyclerViewAdapterHelper")
-
-                    licenses {
-                        license {
-                            //协议类型
-                            name.value("The MIT License")
-                            url.value("https://github.com/CymChad/BaseRecyclerViewAdapterHelper/blob/master/LICENSE")
-                        }
-                    }
-
-                    developers {
-                        developer {
-                            id.value("limuyang2")
-                            name.value("limuyang")
-                            email.value("limuyang2@hotmail.com")
-                        }
-                    }
-
-                    scm {
-                        connection.value("scm:git@github.com/CymChad/BaseRecyclerViewAdapterHelper.git")
-                        developerConnection.value("scm:git@github.com/CymChad/BaseRecyclerViewAdapterHelper.git")
-                        url.value("https://github.com/CymChad/BaseRecyclerViewAdapterHelper")
-                    }
-                }*/
-            }
-
-        }
-
-        /*repositories {
-            *//*maven {
-                url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-                credentials {
-                    username = ossrhUsername
-                    password = ossrhPassword
-                }
-            }*//*
-        }*/
-
-    }
-
-}
-
-/*
-gradle.taskGraph.whenReady {
-    if (allTasks.any { it is Sign }) {
-
-        allprojects {
-            extra["signing.keyId"] = signingKeyId
-            extra["signing.secretKeyRingFile"] = secretKeyRingFile
-            extra["signing.password"] = signingPassword
-        }
-    }
-}
-*/
-
-signing {
-    sign(publishing.publications)
-}
-
